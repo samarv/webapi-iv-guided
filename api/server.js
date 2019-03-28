037a826e78b1d42686c330657c4d147a7870ed7c
samarv <samarvir1996@gmail.com>
2019-03-28 15:29:31 +0530

    add motd again

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    const messageOfTheDay = process.env.MOTD || 'Hello World!';
+    const messageOfTheDay = process.env.MOTD || "hello world"
     res.status(200).json({ motd: messageOfTheDay, shoutouts });
   } catch (error) {
     console.error('\nERROR', error);
